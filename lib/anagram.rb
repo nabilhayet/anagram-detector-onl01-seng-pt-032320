aaad9fd3ce83f6acb9feec6ffef8d8b3d2cdd485
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-19 00:25:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+attr_accessor :word 
+
+  def initialize(words)
+    @word=words 
+  end
+  
+  def match(str)
+    array=[]
+      str.each do |value|
+        if value.split('').sort == @word.split('').sort 
+          array << value 
+        end 
+      end 
+      array
+  end 
+end 
